c5b63ffc203b7b0df004fd84c72b20f60a839a5d
Joan Perals <joan.perals@ixds.com>
2017-04-29 22:09:31 +0200

    Refactor

--- sastre.js
+++ sastre.js
@@ -1,88 +1,89 @@
 class Sastre {
-    checkWith(fn) {
-        this.check = fn;
+    constructor () {
+        this.actions = {
+            'if': function (tree) {
+                if (tree instanceof Array) {
+                    return this.eval({
+                        'and': tree
+                    });
+                }
+                else if (typeof tree === 'object') {
+                    return this.evalBranch(tree);
+                }
+                else {
+                    return this.check(tree);
+                }
+            }.bind(this),
+            'and': function (tree) {
+                let allTrue = true;
+                let innerBranch;
+                for (const branch of tree) {
+                    if (typeof branch === 'object') {
+                        let booleanExpression = Object.keys(branch)[0];
+                        allTrue = allTrue && this.check(booleanExpression);
+                        innerBranch = branch[booleanExpression];
+                    }
+                    else {
+                        allTrue = allTrue && this.check(branch);
+                    }
+                }
+                if (innerBranch === undefined) {
+                    return allTrue;
+                }
+                else if (typeof innerBranch === 'object') {
+                    return allTrue && this.evalBranch(innerBranch);
+                }
+                else {
+                    return allTrue && innerBranch;
+                }
+            }.bind(this),
+            'or': function (tree) {
+                let someTrue = false;
+                let innerBranch;
+                for (const branch of tree) {
+                    if (typeof branch === 'object') {
+                        let booleanExpression = Object.keys(branch)[0];
+                        someTrue = someTrue || this.check(booleanExpression);
+                        innerBranch = branch[booleanExpression];
+                    }
+                    else {
+                        someTrue = someTrue || this.check(branch);
+                    }
+                }
+                if (innerBranch === undefined) {
+                    return someTrue;
+                }
+                else if (typeof innerBranch === 'object') {
+                    return someTrue && this.evalBranch(innerBranch);
+                }
+                else {
+                    return someTrue && innerBranch;
+                }
+            }.bind(this),
+            'not': function (tree) {
+                if (typeof tree === 'object') {
+                    let booleanExpression = Object.keys(tree)[0];
+                    let innerExpression = tree[booleanExpression];
+                    return !this.check(booleanExpression) && this.eval(innerExpression);
+                }
+                else {
+                    return !this.check(tree);
+                }
+            }.bind(this)
+        };
     }
     eval(object) {
-        if (typeof object !== 'object') {
-            return object;
-        }
-        const conjunction = Object.keys(object)[0];
-        if (conjunction === 'if') {
-            if (object.if instanceof Array) {
-                return this.eval({
-                    'and': object.if
-                });
-            }
-            else if (typeof object.if === 'object') {
-                return this.evalBranch(object.if);
-            }
-            else {
-                return this.check(object.if);
+        if (typeof object === 'object') {
+            const conjunction = Object.keys(object)[0];
+            if (typeof this.actions[conjunction] === 'function') {
+                return this.actions[conjunction](object[conjunction]);
             }
         }
-        else if (conjunction === 'and') {
-            let allTrue = true;
-            let innerBranch;
-            for (const branch of object.and) {
-                if (typeof branch === 'object') {
-                    let booleanExpression = Object.keys(branch)[0];
-                    allTrue = allTrue && this.check(booleanExpression);
-                    innerBranch = branch[booleanExpression];
-                }
-                else {
-                    allTrue = allTrue && this.check(branch);
-                }
-            }
-            if (innerBranch === undefined) {
-                return allTrue;
-            }
-            else if (typeof innerBranch === 'object') {
-                return allTrue && this.evalBranch(innerBranch);
-            }
-            else {
-                return allTrue && innerBranch;
-            }
-        }
-        else if (conjunction === 'or') {
-            let someTrue = false;
-            let innerBranch;
-            for (const branch of object.or) {
-                if (typeof branch === 'object') {
-                    let booleanExpression = Object.keys(branch)[0];
-                    someTrue = someTrue || this.check(booleanExpression);
-                    innerBranch = branch[booleanExpression];
-                }
-                else {
-                    someTrue = someTrue || this.check(branch);
-                }
-            }
-            if (innerBranch === undefined) {
-                return someTrue;
-            }
-            else if (typeof innerBranch === 'object') {
-                return someTrue && this.evalBranch(innerBranch);
-            }
-            else {
-                return someTrue && innerBranch;
-            }
-        }
-        else if (conjunction === 'not') {
-            if(typeof object.not === 'object') {
-                let booleanExpression = Object.keys(object.not)[0];
-                let innerExpression = object.not[booleanExpression];
-                return !this.check(booleanExpression) && this.eval(innerExpression);
-            }
-            else {
-                return !this.check(object.not);
-            }
-        }
-        else {
-            return object;
-        }
+        return object;
     }
     evalBranch (branch) {
         let booleanExpression = Object.keys(branch)[0];
-        if(['and', 'or', 'not'].indexOf(booleanExpression) === -1) {
+        if(Object.keys(this.actions).indexOf(booleanExpression) === -1) {
             let childBranch = branch[booleanExpression];
             return this.check(booleanExpression) && this.eval(childBranch);
         }
@@ -100,6 +101,9 @@ class Sastre {
         else {
             return true;
         }
+    }
+    checkWith(fn) {
+        this.check = fn;
     }
 }
 
