0802aaaff6ee5ccd234c95a704754ce76f629aa0
Joan Perals <joan.perals@ixds.com>
2018-05-08 22:31:56 +0200

    Add test for JSON syntax trees

--- test/test.js
+++ test/test.js
@@ -2,8 +2,9 @@ const assert = require('assert')
 const yaml = require('yamljs')
 const Sastre = require('../sastre.js')
 
+const testCasesJson = require('./test-cases.json')
 // yaml.load seems to expect path relative to cwd, not to script's location
-const testCases = yaml.load('./test/test-cases.yaml');
+const testCasesYaml = yaml.load('./test/test-cases.yaml');
 
 const sastre = new Sastre();
 
@@ -22,9 +23,16 @@ const expectedResults = [
 ]
 
 describe('Sastre', function () {
+    it('should interpret a set of JSON syntax trees correctly', function () {
+        for (let i = 0; i < testCasesJson.length; i++) {
+            const testCase = testCasesJson[i]
+            const result = sastre.parse(testCase)
+            assert.deepStrictEqual(result, expectedResults[i])
+        }
+    })
     it('should interpret a set of yaml syntax trees correctly', function () {
-        for (let i = 0; i < testCases.length; i++) {
-            const testCase = testCases[i]
+        for (let i = 0; i < testCasesYaml.length; i++) {
+            const testCase = testCasesYaml[i]
             const result = sastre.parse(testCase)
             assert.deepStrictEqual(result, expectedResults[i])
         }
